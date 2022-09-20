dbcdd9b6b96c059a33de6751c303ba1f609359bf
Josh Davenport-Smith <josh.davenport@tghp.co.uk>
2022-09-20 12:39:46 +0100

    Removed unused types

--- types.d.ts
+++ types.d.ts
@@ -1,11 +1,1 @@
 /// <reference types="vite-plugin-svgr/client" />
-
-type VitepressOptions = {
-    root: string;
-    react?: boolean;
-    preact?: boolean;
-}
-
-export default vitepress = (options: VitepressOptions): Promise => {
-
-}
